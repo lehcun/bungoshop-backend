f0fe0f2f7a4b87ed37c0b3bdd2d0402dc5198f58
lehcun <lehcun1099@gmail.com>
2026-03-16 22:23:02 +0700

    feat(cors): update CORS configuration to allow multiple origins

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3000', // cho phép frontend
+    origin: ['http://localhost:3000', 'http://89.167.124.206:3000'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true, // nếu cần cookie / Authorization header
+    credentials: true,
   });
 
   app.use(cookieParser());
