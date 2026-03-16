4fb2b820ea01d3c3cce9aef1c12eb5339375f60f
lehcun <lehcun1099@gmail.com>
2026-03-16 22:38:40 +0700

    feat(cors): update CORS origins to include www and https variants for bungoshop.io.vn

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,10 @@ async function bootstrap() {
     origin: [
       'http://localhost:3000',
       'http://89.167.124.206:3000',
-      'http://bungoshop.io.vn:3000',
+      'http://bungoshop.io.vn', // Thêm tên miền của bạn
+      'https://bungoshop.io.vn', // Thêm bản https luôn cho bước sau
+      'http://www.bungoshop.io.vn',
+      'https://www.bungoshop.io.vn',
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
