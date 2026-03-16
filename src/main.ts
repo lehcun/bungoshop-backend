0fe81e6295a357b8445b6d153d4171f9d9fea56d
lehcun <lehcun1099@gmail.com>
2026-03-16 22:34:19 +0700

    feat(cors): add 'http://bungoshop.io.vn' to CORS allowed origins

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://89.167.124.206:3000'],
+    origin: [
+      'http://localhost:3000',
+      'http://89.167.124.206:3000',
+      'http://bungoshop.io.vn',
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
